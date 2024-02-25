44b42c0a742d6fd5cc20cc1318ef9263c81cdac3
Jaijer <3lims96@gmail.com>
2024-02-25 10:29:37 -0500

    Added the login screen xml, Fixed the splash screen file placement, Manifest

--- app/src/main/java/com/example/cs2340_project/views/LoginActivity.java
+++ app/src/main/java/com/example/cs2340_project/views/LoginActivity.java
@@ -1,4 +1,16 @@
 package com.example.cs2340_project.views;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cs2340_project.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
 }
