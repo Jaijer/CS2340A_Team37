74663b1dd8d168fc08f9e047084eb2bf5464d4ec
Micah-reeves <melonw63@gmail.com>
2024-02-25 21:22:22 -0500

    Merging the 5 feature screens w/o nav bar implementation

--- app/src/main/java/com/example/cs2340_project/views/HomeActivity.java
+++ app/src/main/java/com/example/cs2340_project/views/HomeActivity.java
@@ -1,4 +1,32 @@
 package com.example.cs2340_project.views;
 
-public class HomeActivity {
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cs2340_project.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView headerTextView;
+    private Button recipeActivityButton;
+    private Button ingredientActivityButton;
+    private Button inputMealActivityButton;
+    private Button shoppingListActivityButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity_screen);
+        headerTextView = findViewById(R.id.headerTextView);
+
+    }
+
 }
--- app/src/main/java/com/example/cs2340_project/views/ShoppingListActivity.java
+++ app/src/main/java/com/example/cs2340_project/views/ShoppingListActivity.java
@@ -1,4 +1,27 @@
 package com.example.cs2340_project.views;
 
-public class ShoppingListActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cs2340_project.R;
+
+public class ShoppingListActivity extends AppCompatActivity {
+    private TextView shoppingListHeaderTextView;
+    private Button recipeActivityButton;
+    private Button ingredientActivityButton;
+    private Button inputMealActivityButton;
+    private Button shoppingListActivityButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopping_list_activity_screen);
+        shoppingListHeaderTextView = findViewById(R.id.shoppingListHeaderTextView);
+
+    }
 }
