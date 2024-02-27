d207bc728a712779e0558ae126b7040b77aaacb0
druiz365 <druiz35@gatech.edu>
2024-02-27 13:04:25 -0500

    Added a close button

--- app/src/main/java/com/example/cs2340_project/views/LoginActivity.java
+++ app/src/main/java/com/example/cs2340_project/views/LoginActivity.java
@@ -6,11 +6,20 @@ import android.os.Bundle;
 
 import com.example.cs2340_project.R;
 
+import android.widget.Button;
+import android.view.View;
+
+import com.example.cs2340_project.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+
+        // Add code to handle the close button click
+        Button closeButton = findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(v -> finish());
     }
 }
