d8da1eca24e1b7a8a2b043711caa25e422fd79b4
Micah-reeves <melonw63@gmail.com>
2024-03-29 13:01:08 -0400

    Second half of Ingredient Form

--- app/src/main/java/com/example/cs2340_project/views/IngredientForm.java
+++ app/src/main/java/com/example/cs2340_project/views/IngredientForm.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.cs2340_project.R;
+import com.example.cs2340_project.viewmodels.FoodDatabase;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,8 @@ public class IngredientForm extends AppCompatActivity {
     private EditText name;
     private EditText quantity;
     private EditText calories;
+    private FoodDatabase foodDatabase;
+    private FirebaseAuth auth;
 
 
     @Override
@@ -26,6 +31,8 @@ public class IngredientForm extends AppCompatActivity {
         calories = findViewById(R.id.caloriesIng);
         Button addBtn = findViewById(R.id.addIngBtn);
         Button backBtn = findViewById(R.id.backIngBtn);
+        foodDatabase = FoodDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         addBtn.setOnClickListener(v -> {
             addIngredient();
@@ -38,5 +45,62 @@ public class IngredientForm extends AppCompatActivity {
     }
 
     private void addIngredient() {
+        String nameText = name.getText().toString().trim();
+        String quantityText = quantity.getText().toString().trim();
+        String calorieText = calories.getText().toString().trim();
+
+        if (nameText.isEmpty() || quantityText.isEmpty() || calorieText.isEmpty()) {
+            Toast.makeText(IngredientForm.this, "Please fill in all fields.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int intQuantityText;
+
+        try {
+            intQuantityText = Integer.parseInt(quantityText);
+        } catch (NumberFormatException e) {
+            Toast.makeText(IngredientForm.this, "Quantity must include only numbers.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (intQuantityText <= 0) {
+            Toast.makeText(IngredientForm.this, "Quantity must be positive.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int intCalories;
+
+        try {
+            intCalories = Integer.parseInt(calorieText);
+        } catch (NumberFormatException e) {
+            Toast.makeText(IngredientForm.this, "Calories must include only numbers.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (intCalories <= 0) {
+            Toast.makeText(IngredientForm.this, "Calories must be positive.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (auth.getCurrentUser() != null) {
+            String userId = auth.getCurrentUser().getUid();
+
+            foodDatabase.addIngredient(nameText, intQuantityText, intCalories, userId)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(IngredientForm.this, "Ingredient added successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(IngredientForm.this, "Failed to add ingredient", Toast.LENGTH_SHORT).show();
+                        }
+                    });;
+        } else {
+            Toast.makeText(IngredientForm.this, "User not logged in", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
