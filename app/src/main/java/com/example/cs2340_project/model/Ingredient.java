9b48bd4a5a64e3d2ec33cfdb3414fec18254c8b1
druiz365 <druiz35@gatech.edu>
2024-04-21 23:35:23 -0400

    added checkboxes that dump into 'selectedIngredients' list and Log them

--- app/src/main/java/com/example/cs2340_project/model/Ingredient.java
+++ app/src/main/java/com/example/cs2340_project/model/Ingredient.java
@@ -7,6 +7,8 @@ public class Ingredient implements Parcelable{
     private String name;
     private Integer quantity;
     private Integer calories;
+    private boolean selected;
+
     public Ingredient() {
         // The code will break if you remove this.
     }
@@ -65,5 +67,12 @@ public class Ingredient implements Parcelable{
         dest.writeString(name);
         dest.writeValue(quantity);
         dest.writeValue(calories);
+    }
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
     }
 }
